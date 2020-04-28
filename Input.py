250e963c327b50be105bde075953e6974aabae9e
Emmanuel Ivan <64347802+EmmanuelIvan@users.noreply.github.com>
2020-04-28 15:37:50 +0000

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,6 @@
+#Veremos como cantenar las cadenas de texto
+
+Name = raw_input("Introduce tu nombre: ")
+Name_two = raw_input("Introduce tu segundo nombre: ")
+Last_name = raw_input("Introduce tu apellido: ")
+print("Hola " + Name + Name_two + Last_name)
